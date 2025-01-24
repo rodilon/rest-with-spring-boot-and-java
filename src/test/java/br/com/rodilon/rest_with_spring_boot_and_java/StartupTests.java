ed8bf3edf02de4913d9d864c2d71b737ed7e86e3
rodilon <renanodilon@gmail.com>
2025-01-24 12:00:56 -0300

    first steps with spring boot with java

---
+++ src/test/java/br/com/rodilon/rest_with_spring_boot_and_java/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.rodilon.rest_with_spring_boot_and_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
